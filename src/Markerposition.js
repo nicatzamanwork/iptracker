c65292eaa78e77b17b659e178013d67d72704184
nicatzamanwork <webdevelopernicat@gmail.com>
2023-02-14 23:45:43 +0400

    magic touch

--- src/Markerposition.js
+++ src/Markerposition.js
@@ -1,23 +1,45 @@
 import React, { useEffect, useMemo } from "react";
 import { Marker, Popup, useMap } from "react-leaflet";
+// import icon from "./icon";
+
+// export default function Markerposition({ address }) {
+//   const position = useMemo(() => {
+//     return [address.longitude, address.latitude];
+//   }, [address.longitude, address.latitude]);
+//   const map = useMap();
+
+//   useEffect(() => {
+//     map.flyTo(position, 5, {
+//       animate: true,
+//     });
+//   }, [map, position]);
+
+//   return (
+//     <>
+//       <Marker icon={icon} position={position}>
+//         <Popup>This is the location of the IP Address or Domain</Popup>
+//       </Marker>
+//     </>
+//   );
+// }
+
 import icon from "./icon";
 
 export default function Markerposition({ address }) {
-  const position = useMemo(() => {
-    return [address.longitude, address.latitude];
-  }, [address.longitude, address.latitude]);
   const map = useMap();
 
   useEffect(() => {
-    map.flyTo(position, 5, {
+    map.flyTo([address.latitude, address.longitude], 13, {
       animate: true,
     });
-  }, [map, position]);
+  }, [map, address]);
 
   return (
     <>
-      <Marker icon={icon} position={position}>
-        <Popup>This is the location of the IP Address or Domain</Popup>
+      <Marker icon={icon} position={[address.latitude, address.longitude]}>
+        <Popup>
+          {address.country} <br /> {address.city}
+        </Popup>
       </Marker>
     </>
   );
